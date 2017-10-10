7db3c82d6b4735ece3fbcfeb7a37a618421ab88c
abebemukkuru <abebebebeyuuki@gmail.com>
2017-10-10 23:26:59 +0900

    add TitleScene
    タイトルシーンの追加

---
+++ VR_Game/Assets/LocalScript/ButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+    [SerializeField]
+    private string loadName = "TitleScene";
+
+    //クリックしたときの関数
+    public void OnClick(){
+
+        //シーン遷移
+        SceneManager.LoadScene(loadName);
+    }
+}
